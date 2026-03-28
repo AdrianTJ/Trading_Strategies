65f91b18ebbb089bd4f5ac3cd66780b897304d0b
Adrian TJ <adrian.tame.jacobo@gmail.com>
2026-03-28 14:41:21 -0600

    feat(02-01): register all models in session.py

--- src/db/session.py
+++ src/db/session.py
@@ -1,6 +1,11 @@
 import os
 from sqlmodel import create_engine, SQLModel, Session
 
+# Import models to register them
+from src.db.models import Series, Observation
+from src.db.aligned_models import AlignedObservation
+from src.db.results_models import SimulationResult, DailyPerformance
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./trading_strategies.db")
 
 engine = create_engine(DATABASE_URL, echo=False)
