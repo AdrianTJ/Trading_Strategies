dffc59a5e79de02841e878709937e502c3006780
Adrian TJ <adrian.tame.jacobo@gmail.com>
2026-04-05 09:41:44 -0600

    Fixing part 3

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,9 @@
+const tailwindcssPlugin = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwindcssPlugin,
+    autoprefixer,
+  ],
+};
